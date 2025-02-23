5e2cd48936b23fdb60d8148c341ff276130e576f
Priyanshu Trivedi <priyanshutrivedi.7026@gmail.com>
2025-02-23 12:28:28 +0530

    Billing Management System(V0.0.1)

---
+++ lib/view/splash_screen/splash_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            children: [
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
