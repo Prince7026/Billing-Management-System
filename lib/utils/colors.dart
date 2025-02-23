677c95b2d4389c3075fe8e54cbb86e591cc4da3f
Priyanshu Trivedi <priyanshutrivedi.7026@gmail.com>
2025-02-23 21:56:01 +0530

    Billing Management System(V0.0.1)

---
+++ lib/utils/colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+const Color blue50 = Color(0xfff0f5fe);
+const Color blue100 = Color(0xffdee7fb);
+const Color blue200 = Color(0xffc4d7f9);
+const Color blue300 = Color(0xff9bbdf5);
+const Color blue400 = Color(0xff6495ed);
+const Color blue500 = Color(0xff4a78e7);
+const Color blue600 = Color(0xff355adb);
+const Color blue700 = Color(0xff2c47c9);
+const Color blue800 = Color(0xff293ba4);
+const Color blue900 = Color(0xff263682);
+const Color blue950 = Color(0xff1c234f);
